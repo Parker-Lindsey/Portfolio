091620ce9ac548b2486d3eb220316673bd7869ba
Parker <plindsey1496@gmail.com>
2019-06-15 20:24:00 -0500

    Working on portfolio site

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   entry: {
-    App: "./app/assets/scripts/App.js",
-    Vendor: "./app/assets/scripts/Vendor.js"
+    App: "C:\Users\Parker\Desktop\Sites\portfolio\app\assets\scripts\App.js",
+    Vendor: "C:\Users\Parker\Desktop\Sites\portfolio\app\assets\scripts\Vendor.js"
   },
   output: {
-    path: "./app/temp/scripts",
+    path: "C:\Users\Parker\Desktop\Sites\portfolio\app\temp\scripts",
     filename: "[name].js"
   },
   module: {
-    loaders: [
+    rules: [
       {
         loader: 'babel',
         query: {
